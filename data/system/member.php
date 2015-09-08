f631d4357c836d009f6525e19c1b1a156a64ef8a
james.xiong <james.xiong@daocloud.io>
2015-09-08 11:45:13 +0800

    init commit

---
+++ data/system/member.php
@@ -0,0 +1,1 @@
+<?php exit;?>{"admin":{"name":"admin","password":"21232f297a57a5a743894a0e4a801fc3","role":"root","status":0},"demo":{"name":"demo","password":"fe01ce2a7fbac8fafaed7c982a04e229","role":"default","status":0}}
